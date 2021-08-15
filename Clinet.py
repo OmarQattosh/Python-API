12862e52d30f512db0ddf702c6cdeb1cae1c687a
Omar Qattosh <OmarQ@exalt.ps>
2021-08-15 12:43:04 +0300

    Latest

--- Clinet.py
+++ Clinet.py
@@ -1,2 +1,14 @@
+from flask.wrappers import Response
+import requests
+
 import psutil
-print(psutil.cpu_percent())
+from werkzeug.wrappers import response
+
+Base="http://127.0.0.1:5000/"
+res=psutil.cpu_percent()
+
+response= requests.put(Base+"cpu/1",{"cpuperc":res})
+print(response.json())
+input()
+response= requests.get(Base+"cpu/1")
+print(response.json())
--- Server.py
+++ Server.py
@@ -1,25 +1,27 @@
 from flask import Flask,request
-from flask.wrappers import Request
+from flask.wrappers import Request, Response
 from flask_restful import Api,Resource, reqparse
 app = Flask(__name__)
 api= Api(app)
 
 cpu_put_args = reqparse.RequestParser()
-cpu_put_args.add_argument("cpuUsage",type=str,help="The Cpu Usage",)
+cpu_put_args.add_argument("cpuUsage",type=str,help="The Cpu Usage")
+cpuu = {}
+class home(Resource):
+    def get(self):
+        return "Test Ramy"
 class cpu(Resource):
-    def put(self,cpu_use):
-        return 
+    def get (self,cpu_id):
+        return cpu[cpu_id]
+    def put(self,cpu_id):
+        args=cpu_put_args.parse_args()
+        cpuu[cpu_id]=args
+        return cpuu[cpu_id],201
 
 
+api.add_resource(home,"/")
+api.add_resource(cpu,"/cpu/<int:cpu_id>")
 
-
-
-
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>test if this api works or not</h1>
-<p>Testing</p>'''
-
-api.add_resource(cpu,"/cpu")
+#api.add_resource(cpu,"/cpu")
 if __name__=="__main__":
     app.run(debug=True)
